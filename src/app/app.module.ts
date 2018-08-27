fa1a5f7454c2915cc3462ad8135d7cac7ad83899
Khim22 <khimyang22@gmail.com>
2018-08-27 16:19:51 +0800

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './components/category.component';
+import { CategoryItemComponent } from './components/category-item.component';
+import { ItemDetailsComponent } from './components/item-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    CategoryItemComponent,
+    ItemDetailsComponent,
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/model.ts
@@ -0,0 +1,1 @@
+//interface codes here
