9a510e15cc66391fd2ed612d5dff7883b5792d55
Khim22 <khimyang22@gmail.com>
2018-12-11 21:26:12 +0800

    initial commit to routing

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const path = require('path')
+const http = require('http')
+const bodyParser = require('body-parser')
+
+const api = require('./server/routes/api')
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(express.static(path.join(__dirname,'src')))
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'src/app.component.html'))
+});
+
+const port = process.env.PORT || '4000';
+
+app.set('port',port)
+
+const server = http.createServer(app);
+
+
+server.listen(port, ()=> console.log('app running on port ${port}'))
+
+
+
---
+++ src/app/starwars.service.ts
@@ -0,0 +1,7 @@
+import { Injectable, Injectable } from '@angular/core'
+import { HttpClient, HttpParams } from '@angular/common/http'
+
+@Injectable
+export class StarWarsService{
+    
+}
