d576279d8a7fcca4af29af799410df0190f7676f
meitingtonia <meitingtonia@gmail.com>
2019-01-21 17:12:34 +0800

    add share button

--- src/app/components/category.component.ts
+++ src/app/components/category.component.ts
@@ -1,29 +1,40 @@
 import { Component, OnInit } from "@angular/core";
 import { StarWarsService } from "../starwars.service";
-
-
 import { Router } from "@angular/router";
 import { Location } from "@angular/common";
-// import { element } from "../../../node_modules/@angular/core/src/render3/instructions";
-
-
-
+import { SocialService} from "ng6-social-button";
 @Component({
   selector: "app-category",
   templateUrl: "./category.component.html",
   styleUrls: ["./category.component.css"]
 })
+
 export class CategoryComponent {
   cat = [];
   displayedColumns: string[] = ["name"];
   dataSource;
   selectedRowIndex;
-
+  shareObj = {
+    href: "http://localhost:4200/",
+    hashtag:"#FACEBOOK-SHARE-HASGTAG"
+   };
   constructor(
     private router: Router,
     private _location: Location,
-    private starwarservice: StarWarsService
+    private starwarservice: StarWarsService,
+    private socialAuthService: SocialService
   ) {}
+  signOut(){
+    if(this.socialAuthService.isSocialLoggedIn()){
+        this.socialAuthService.signOut().catch((err)=>{
+
+        });
+    }
+ }
+
+ getSocialUser(socialUser){
+     console.log(socialUser);
+ }
   ngOnInit() {
     this.starwarservice.getAllCategories().then(result => {
       if(result)
@@ -34,10 +45,7 @@ export class CategoryComponent {
     
   }
  
-  // goToCategoryItem() {
-  //   this.starwarservice.setItemSelected("planets");
-  //   this.router.navigate(["categoryItems"]);
-  // }
+  
   highlight(row){
     this.selectedRowIndex = row.name;
     this.starwarservice.setItemSelected(this.selectedRowIndex);
